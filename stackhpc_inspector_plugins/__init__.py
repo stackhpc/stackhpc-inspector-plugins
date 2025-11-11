00419b6836fa2ec72af3a1534a82f440ae62a098
John Garbutt <john.garbutt@stackhpc.com>
2025-11-11 18:17:09 +0000

    Move oslo fix to just unit tests

--- stackhpc_inspector_plugins/__init__.py
+++ stackhpc_inspector_plugins/__init__.py
@@ -15,7 +15,3 @@
 
 # Import configuration module to register configuration options.
 from stackhpc_inspector_plugins import conf  # noqa: F401
-
-# NOTE(TheJulia): This is to force oslo_service from trying to use eventlet.
-from oslo_service import backend
-backend.init_backend(backend.BackendType.THREADING)
